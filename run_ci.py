4f1ee98db29faae8a6638c2d904f2ff6e8df4faa
menify <menify@gmail.com>
2015-09-01 02:00:52 +0300

    Fixing CI scripts

---
+++ run_ci.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+# ==============================================================================
+
+def _run_cmd(cmd, path=None):
+
+    if path:
+        env = os.environ.copy()
+        env['PYTHONPATH'] = path
+    else:
+        env = None
+
+    print(cmd)
+
+    p = subprocess.Popen(cmd, env=env, shell=False)
+    result = p.wait()
+
+    if result:
+        sys.exit(result)
+
+
+# ==============================================================================
+
+def _run_example(core_dir, tools_dir, example_dir):
+    tools_dir = os.path.join(tools_dir, 'tools')
+
+    cmd = ["python", "-c", "import aql;import sys;sys.exit(aql.main())", "-C", example_dir, "-I", tools_dir]
+    _run_cmd(cmd, core_dir)
+    _run_cmd(cmd + ['-R'], core_dir)
+
+
+# ==============================================================================
+
+def _get_examples(examples_dir):
+    for item in os.listdir(examples_dir):
+        example_dir = os.path.join(examples_dir, item)
+        makefile = os.path.join(example_dir, 'make.aql')
+        if os.path.isfile(makefile):
+            yield example_dir
+
+
+# ==============================================================================
+
+def run(core_dir, tools_dir, examples_dir):
+    for example_dir in _get_examples(examples_dir):
+        _run_example(core_dir, tools_dir, example_dir)
+
+
+# ==============================================================================
+
+def main():
+    examples_dir = os.path.abspath(os.path.dirname(__file__))
+    core_dir = "/home/me/work/src/aqualid/aqualid"
+    tools_dir = "/home/me/work/src/aqualid/tools"
+
+    # tools_dir = os.path.join(examples_dir, 'tools')
+    # core_dir = os.path.join(examples_dir, 'aqualid')
+    #
+    # _run_cmd(["git", "clone", "-b", "pytest", "--depth", "1", "https://github.com/aqualid/aqualid.git"])
+    # _run_cmd(["git", "clone", "-b", "pytest", "--depth", "1", "https://github.com/aqualid/tools.git"])
+
+    run(core_dir, tools_dir, examples_dir)
+
+
+# ==============================================================================
+if __name__ == '__main__':
+    main()
